637b0c0197789fcb46f824e4942efd32f5c04fb6
natashagomesr <natashagomesr@gmail.com>
2022-07-20 18:18:44 -0300

    Alteraçoes no router

--- future-eats/src/Router/Router.js
+++ future-eats/src/Router/Router.js
@@ -1,16 +1,20 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom"
-import Cadastro from "../Pages/Cadastro/Cadastro"
-import Login from "../Pages/Login/Login"
-import MeuEndereço from "../Pages/MeuEndereço/MeuEndereço"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Cadastro from "../Pages/Cadastro/Cadastro";
+import Home from "../Pages/Home/home";
+import Login from "../Pages/Login/Login";
+import MeuEndereço from "../Pages/MeuEndereço/MeuEndereço";
 
-const Router=()=>{
-    return (
-        <BrowserRouter>
-        <Routes>
-            <Route index element={<Login/>} />
-            <Route path="/Cadastro" element={<Cadastro/>}/>
-            <Route path="/MeuEndereço" element={<MeuEndereço/>}/>
-        </Routes>
-        </BrowserRouter>
-    )
-}
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Login />} />
+        <Route path="/Cadastro" element={<Cadastro />} />
+        <Route path="/MeuEndereço" element={<MeuEndereço />} />
+        <Route index path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
